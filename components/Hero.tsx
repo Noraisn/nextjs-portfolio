340e12c426b77ac83d29681dd4d1efac161ad07e
Noraisn <noraiseni6972@gmail.com>
2025-02-11 14:50:24 +0100

    Updated Ui

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -31,8 +31,8 @@ export const Hero = () => {
 
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-          <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Dynamic Web Magic with Next.js
+          <p className="uppercase tracking-widest text-xs text-center font-bold text-red-500 max-w-80">
+            Portfolio is in Progress
           </p>
           <TextGenerateEffect
             words="Transforming Concepts into Seamless User Experiences"
@@ -40,7 +40,7 @@ export const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Adrian, a Next.js Developer based in Croatia.
+            Hi! I&apos;m Nora, a Next.js Developer based in Pristina, Kosovo.
           </p>
 
           <a href="#about">
